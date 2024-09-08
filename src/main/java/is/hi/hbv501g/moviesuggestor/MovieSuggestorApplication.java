62bd9d163eb4fba64ca719debac99fe0597f196c
katrin108 <kas108@hi.is>
2024-09-08 16:00:00 +0000

    “initial commit”

---
+++ src/main/java/is/hi/hbv501g/moviesuggestor/MovieSuggestorApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.moviesuggestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSuggestorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieSuggestorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/is/hi/hbv501g/moviesuggestor/MovieSuggestorApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.moviesuggestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieSuggestorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
